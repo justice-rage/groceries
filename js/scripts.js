82b011df090bbb5716d5bb22ccf814f211c30805
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-30 16:25:12 -0800

    add uppercase and alphebetical array logic
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-  $("form#iceCream").submit(function(event) {
-    const iceCreamArray = ["Pistachio", "Strawberry", "Chocolate","Vanilla"];
-    iceCreamArray.forEach(function(thing) {
-      $("#flavor").append(thing);
+  // grocery array
+  $("form#grocery").submit(function(event) {
+    const groceries = [$("input#item-one").val(), $("input#item-two").val(), $("input#item-three").val()];  
+
+    //alphebetical ordered array
+    const alphebetArray = groceries.sort();
+    
+    // capitalize array
+    const uppercased = alphebetArray.map(name => name.toUpperCase());
+
+    // const upperCaseArray = alphebetArray.toUpperCase();
+    
+    // output mutated array
+    uppercased.forEach(function(thing) {
+      $("#final-result").append(thing);
     })
     event.preventDefault();
   });
